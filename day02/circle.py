457e46a7fc81133d26a150db35795bb95415f6ef
Anvita Pant <pant.anvita@gmail.com>
2025-11-02 18:10:25 +0200

    new code to calcuate area

---
+++ day02/circle.py
@@ -0,0 +1,36 @@
+# Program to calculate the diameter, area and circumference of a circle from user input radius value
+
+print("This program will calculate the diameter, area and circumference of a circle having a user-specified radius.")
+
+# To check if radius is a positive number and not a string
+
+def circle_area(radius):
+    """Calculate the area of a circle"""
+    while True:
+        try:
+            value= float(input(radius))
+            if value <0:
+                print("Please enter a positive number")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input! Please enter a numeric value")
+
+# Taking user input
+
+radius= circle_area("Enter the radius of the circle: ")
+
+# Formula to calculate area and circumference and diameter
+    
+diameter= 2 * radius
+area= 3.14 * radius * radius
+circumference= 2 * 3.14 * radius
+
+# Print the outputs
+
+print(f"The diameter of the circle is: {diameter} units")
+print(f"The area of the circle is: {area} sq units")
+print(f"The circumference of the circle is: {circumference} units")
+
+
+
---
+++ day02/rectangle.py
@@ -0,0 +1,44 @@
+# Program to calculate the area and perimeter of a rectangle (if-else-elif statements)
+
+print("This program will calculate the area and perimeter of a rectangle having a user-specified length and width.")
+
+# To check if length and width are positive numbers and not strings
+
+def rectangle_areaperi(userinput):
+    """Calculate the area and perimeter of a rectangle."""
+    while True:
+        try:
+            value= float(input(userinput))
+            if value <=0:
+                print("Please enter a positive number greater than zero")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input! Please enter a numeric value")
+
+# Taking user input
+
+length= rectangle_areaperi("Enter the length of the rectangle: ")
+width= rectangle_areaperi("Enter the width of the rectangle: ")
+
+# Formula to calculate area and perimeter
+
+area= length * width
+
+perimeter= 2 * (length + width)
+
+# Print the outputs
+
+print(f"The area of the rectangle is: {area} sq units")
+print(f"The perimeter of the rectangle is: {perimeter} units")
+
+# Compare area and perimeter
+
+if area > perimeter:
+    print("The area is greater than the perimeter.")
+elif area < perimeter:
+    print("The perimeter is greater than the area.")
+else:
+    print("The area and perimeter are equal.")
+
+
